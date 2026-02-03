c0f7c1a82e2f097d8ea4eb498ae1e882d77bed6c
abishaekduresh <abishaekduresh@gmail.com>
2026-02-03 15:17:11 +0530

    chore: remove legacy frontend folder

--- frontend/app/reseller/layout.tsx
+++
@@ -1,5 +0,0 @@
-import AdminLayout from '@/components/AdminLayout';
-
-export default function ResellerLayout({ children }: { children: React.ReactNode }) {
-  return <AdminLayout>{children}</AdminLayout>;
-}
--- frontend/components/NetworkStatusMonitor.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useNetworkStatus } from '@/hooks/useNetworkStatus';
-
-export default function NetworkStatusMonitor() {
-  useNetworkStatus();
-  return null;
-}
